5a090fbc2be22b60befa327c17d2467fa703dae0
yamdev07 <yamdyamd080@gmail.com>
2026-01-12 11:52:05 +0100

    deleted next exports

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true
-  }
+    domains: [],
+  },
 };
 
 module.exports = nextConfig;
