a9a289e059b0f5caff7601c9f16f1799455fb273
yamdev07 <yamdyamd080@gmail.com>
2026-01-12 13:51:12 +0100

    maj dependeces

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: [],
+    unoptimized: true,
   },
+  // Optionnel : pour éviter certaines erreurs
+  trailingSlash: true,  // Ajoutez ceci si vous avez des problèmes de routes
 };
 
 module.exports = nextConfig;
